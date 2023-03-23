a25c56d1996533db4d6925f0af1234887e57f261
vitoria <vi21cgomes@gmail.com>
2023-03-23 20:20:16 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Buscador de CEP</h1>
+      
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu cep"/> 
+      </div>
+    </div>
+  );
+}
+
+export default App;

eb09f9904ef106418c70cd1c11646b00b16974fc
vitoria <vi21cgomes@gmail.com>
2023-03-23 21:20:40 -0300

    style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
+import {FiSearch} from 'react-icons/fi';
+import './style.css'
 function App() {
   return (
     <div className="container">
-      <h1>Buscador de CEP</h1>
+      <h1 className='titulo'>Buscador de CEP</h1>
       
       <div className="containerInput">
         <input type="text" placeholder="Digite seu cep"/> 
+
+        <button className="botaodeprocura">
+          <FiSearch size={16} color='000'/>
+        </button>
       </div>
+      <main className='main'>
+        <h2> CEP: 63982638</h2>
+        <span> Rua Lugar Nenhum</span>
+        <span> Complemento: numero 38</span>
+        <span> Caxanga</span>
+        <span> Recife-PE</span>
+      </main>
     </div>
   );
 }
